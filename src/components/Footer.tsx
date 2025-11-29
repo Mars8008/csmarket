da493b65db7ebdf7cbf97ad8d9a3f5ed510da8d6
Angarag Ulziibadrakh <angaragulziibadrakh@dyn-10-140-168-202.d.umn.edu>
2025-11-29 02:07:41 -0600

    up

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,9 @@
 export default function Header() {
     return(
-        <div className="mt-[10px] bg-[#f7f7f7] flex text-black gap-[14px] text-[18px]  mt-[700px] h-[400px] px-3">
-            <div className="container mx-auto max-w-[1200px]">
+        <div className="mt-[10px] bg-[#f7f7f7] flex text-black gap-[14px] text-[18px]  mt-[700px] h-[500px] px-3">
+            <div className="container mx-auto max-w-[1200px] max-sm:text-[14px] text-[20px]">
            <p className=" font-bold max-sm:text-[16px] text-[28px] mt-[40px] mb-3">CSMarket</p>
+           <div className="min-md:flex min-md:justify-between">
            <div>
             <div className="flex max-sm:text-[16px] text-[20px] gap-10 mb-4 ml-0.5">
             <p>Mail:</p>
@@ -16,6 +17,37 @@ export default function Header() {
             <p>Address:</p>
             <p> 549 Oak sr.Krystal</p>
             </div>
+           </div>
+           
+            <div>
+                <p className="font-bold">INFORMATION</p>
+                <p>Contact us</p>
+                <p>Career</p>
+                <p>My Account</p>
+                <p>Order & Returns</p>
+                <p>FaQs</p>
+            </div>
+            <div>  <p className="font-bold">QUICK SHOP</p>
+                  <p>Knives</p>
+                  <p>Gloves</p>
+                  <p>Rifles</p>
+                  <p>Pistols</p>
+                  <p>SMGs</p>
+                  <p>Shotguns</p>
+                  <p>Machineguns</p>
+                  <p>Stickers</p>
+                  <p>Charm</p>
+                  <p>Agent</p>
+                  <p>Others</p>
+            </div>
+
+            <div> <p className="font-bold">CUSTOMER SERVICES</p>
+                  <p>Orders FAQs</p>
+                  <p>Shipping</p>
+                  <p>Privacy Policy</p>
+                  <p>Return & Refund</p>
+            </div>
+           
            </div>
            </div>
            
