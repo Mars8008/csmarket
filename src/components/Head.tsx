139462b5ca09f2ba1e9c16fb9afff6db344fd258
Angarag Ulziibadrakh <angaragulziibadrakh@dyn-10-140-168-202.d.umn.edu>
2025-11-29 00:33:06 -0600

    update

---
+++ src/components/Head.tsx
@@ -0,0 +1,7 @@
+export default function Head() {
+    return(
+        <div className="bg-black  ">
+            <p className=" text-[14px] flex justify-center h-[40px] py-3">NEW CUSTOMERS SAVE 10% WITH THE CODE CSSKIN</p>
+        </div>
+    )
+}
